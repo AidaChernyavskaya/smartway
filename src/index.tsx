c28923015e6a66f09627c497ac6f570a416e176b
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2024-12-08 23:47:56 +0300

    Change router for github pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {BrowserRouter} from "react-router";
+import {HashRouter} from "react-router";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>
+    </HashRouter>
 );
 
